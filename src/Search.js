0108c62c2e9669a96a155cd14bc96fecd1a9a5d6
cisvercouteren <cis.vercouteren@tntap.be>
2018-01-15 15:33:34 +0100

    ref test

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Search.css";
 
-const Search = ({ onChangeSearch, searchValue, onSubmit, children }) => {
-    return (
-        <form onSubmit={onSubmit}>
-            <input type="text" onChange={onChangeSearch} value={searchValue} />
-            <button type="submit">{children}</button>
-        </form>
-    );
-};
+class Search extends Component {
+    componentDidMount = () => {
+        this.input.classList.add("customInput");
+        this.input.focus();
+    };
+    render() {
+        const { onChangeSearch, searchValue, onSubmit, children } = this.props;
+        return (
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    onChange={onChangeSearch}
+                    value={searchValue}
+                    ref={node => {
+                        this.input = node;
+                    }}
+                />
+                <button type="submit">{children}</button>
+            </form>
+        );
+    }
+}
 
 export default Search;
